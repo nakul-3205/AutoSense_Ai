c527185e99a0e2b6c9c598c82d9203b510b09ada
nakul-3205 <nakulkejriwal079@gmail.com>
2025-09-27 18:35:26 +0530

    decompressed model

---
+++ decompress.py
@@ -0,0 +1,11 @@
+import joblib
+
+# Load the compressed model
+compressed_path = "artifacts/09_27_2025_17_47_09/model_trainer/trained_model/model.pkl"  # your compressed file
+model = joblib.load(compressed_path)
+
+# Save it again uncompressed
+decompressed_path = "best_model/model.pkl"
+joblib.dump(model, decompressed_path, compress=0)  # compress=0 means no compression
+
+print(f"Decompressed model saved at: {decompressed_path}")
