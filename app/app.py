6c1504096e6b1cfba54de4481957a9194685314a
nakul-3205 <nakulkejriwal079@gmail.com>
2025-09-29 14:14:43 +0530

    fix

--- app/app.py
+++ app/app.py
@@ -1,23 +1,18 @@
 from flask import Flask, render_template, request
-import pickle
-import numpy as np
 import os
-import joblib
 import pandas as pd
+import pickle
+import joblib
 
 app = Flask(__name__)
-
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PREPROCESSOR_PATH = os.path.join(BASE_DIR, "..", "best_model", "transformed_object", "preprocessing.pkl")
 MODEL_PATH = os.path.join(BASE_DIR, "..", "best_model", "model.pkl")
 
-
-with open(PREPROCESSOR_PATH, "rb") as f:
-    preprocessor = pickle.load(f)
-
-model = joblib.load(MODEL_PATH)
-
+# Lazy load objects
+preprocessor = None
+model = None
 
 FEATURE_COLUMNS = [
     "transmission",
@@ -30,11 +25,21 @@ FEATURE_COLUMNS = [
     "vehicle_age"
 ]
 
+def load_model_objects():
+    global preprocessor, model
+    if preprocessor is None:
+        with open(PREPROCESSOR_PATH, "rb") as f:
+            preprocessor = pickle.load(f)
+    if model is None:
+        model = joblib.load(MODEL_PATH)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     prediction = None
     if request.method == "POST":
         try:
+            load_model_objects()  # lazy load
+
             # Collect user input
             user_input = {
                 "transmission": request.form["transmission"],
@@ -47,10 +52,7 @@ def index():
                 "vehicle_age": float(request.form["vehicle_age"])
             }
 
-
             input_df = pd.DataFrame([user_input], columns=FEATURE_COLUMNS)
-
-
             input_transformed = preprocessor.transform(input_df)
 
             # Predict
@@ -64,4 +66,4 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
